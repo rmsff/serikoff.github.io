01d0e47125471cee39574b22f470dc7f1a3af4d2
Serikoff <serikoff@li.ru>
2020-02-13 15:12:30 +0300

    add table

---
+++ table/precache-manifest.5767e5137f7217d79cef053595512044.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17f9c1c3c91ec084d086348b09b3d0a5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "30acf0b508a9f837dac2",
+    "url": "/static/css/2.9ddb28ea.chunk.css"
+  },
+  {
+    "revision": "d5a38f57c1618c5191b3",
+    "url": "/static/css/main.65938976.chunk.css"
+  },
+  {
+    "revision": "30acf0b508a9f837dac2",
+    "url": "/static/js/2.6fac1efd.chunk.js"
+  },
+  {
+    "revision": "cf2a3117f19ae3bdf475c0861ba4a600",
+    "url": "/static/js/2.6fac1efd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5a38f57c1618c5191b3",
+    "url": "/static/js/main.043637f9.chunk.js"
+  },
+  {
+    "revision": "f95cb11d985f3a2bb642",
+    "url": "/static/js/runtime-main.1270bae7.js"
+  }
+]);
